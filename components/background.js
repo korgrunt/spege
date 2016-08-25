5e5578b19dcc0a72004d5a6563c46f047ef65730
informatique2 <informatique2@ihc.local>
2016-08-25 17:57:33 +0200

    25-08-2016 soir

--- components/background.js
+++ components/background.js
@@ -1,2 +1,33 @@
 /* tout les 10 seconde, quelque soi la page visité, lance une pop up a cliquer contenant le msg*/
-setInterval(function () {alert('Page d\'arrière plan en marche');},10000);
+function features_01(){
+  setInterval(function () {alert('Page d\'arrière plan en marche');},10000);
+}
+
+function faireJoujou(fenetre)
+	{
+    /*
+	  setInterval(function () {alert(fenetre);},10000);
+*/
+  }
+/*
+  chrome.tabs.getSelected(null,function(tab) { // null defaults to current window
+    var title = tab.title;
+    faireJoujou(title);
+  });
+*/
+
+
+
+  var myVar;
+
+  function myFunction() {
+      setInterval(alertFunc, 3000);
+  }
+
+  function alertFunc() {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        console.log(tabs[0].url);
+    });
+  }
+
+  myFunction();
